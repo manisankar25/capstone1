a95915f880148c30697fc2658f89d6c80924b806
manisankar25 <sankarkantipudi@gmail.com>
2021-01-18 11:25:38 +0530

    str

--- cap/src/setupTests.js
+++ cap/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+describe('My Fir fdst nTest', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  }) 
